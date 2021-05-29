56348e9eb214183615b42acdb5c143231539ed34
Timothy Popovskiy <penlist127@gmail.com>
2021-05-29 09:37:04 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const sass         = require('gulp-sass');
+const concat       = require('gulp-concat');
+const browserSync  = require('browser-sync').create();
+const uglify       = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin     = require('gulp-imagemin');
+const del          = require('del');
+
+function browsersync(){
+    browserSync.init({
+       server: {
+           baseDir: 'app/', 
+           routes: {
+               '/node_modules': 'node_modules'
+           }
+       } 
+    });
+}
+
+function cleanDist(){
+    return del('dist');
+}
+
+
+
+function scripts() {
+    return src([
+        'node_modules/focus-visible/dist/focus-visible.min.js',
+        'app/js/main.js'
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream())
+}
+
+
+function styles() {
+    return src('app/sass/style.sass')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 version'],
+            grid: true
+        }))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream())
+}
+
+function build() {
+    return src([
+        'app/css/style.min.css',
+        'app/sprite.svg',
+        'app/img/**/*',
+        'app/fonts/**/*',
+        'app/js/main.min.js',
+        'app/*.html'
+    ], {base: 'app'})
+        .pipe(dest('dist'))
+}
+
+function watching() {
+    watch(['app/sass/**/*.sass'], styles);
+    watch(['app/js/**/*.js' , '!app/js/main.min.js'], scripts);
+    watch(['app/*.html']).on('change', browserSync.reload);
+}
+
+
+exports.styles      = styles;
+exports.watching    = watching;
+exports.browsersync = browsersync;
+exports.scripts     = scripts;
+exports.cleanDist   = cleanDist;
+
+exports.build   = series(cleanDist, build);
+exports.default = parallel(styles, scripts, browsersync, watching);
